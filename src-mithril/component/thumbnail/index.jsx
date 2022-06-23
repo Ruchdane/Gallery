5c4efdabaec411ada6a2e6fd637e0e133eedd642
ruchdane <ruchdaneabiodun@gmail.com>
2022-06-23 16:07:06 +0100

    Galery view style update

--- src-mithril/component/thumbnail/index.jsx
+++ src-mithril/component/thumbnail/index.jsx
@@ -10,7 +10,7 @@ const Thumbnail = {
          */
         var galery = vnode.attrs.galery
         return <div class={`thumbnail ${is_undefined(vnode.attrs.class)}`}>
-            <img src={galery.thumbnail}>  </img>
+            <img src="/wallpaper/673189.png">  </img>
             {/* <img src={convertFileSrc(galery.thumbnail)}>  </img> */}
             <div class='description'>
                 <label class='name'> { galery.path.slice(galery.path.lastIndexOf('/')+1)} </label>
