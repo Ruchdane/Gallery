a53a023d78a4118e6e1857624c951452a60c91b8
ruchdane <ruchdaneabiodun@gmail.com>
2022-07-04 14:54:28 +0100

    setup mithril unit testing

---
+++ src-mithril/component/__tests__/test-setup.js
@@ -0,0 +1,20 @@
+import { after } from "ospec"
+import { JSDOM } from "jsdom"
+
+// Require Mithril.js to make sure it loads properly.
+import "mithril"
+const dom = new JSDOM("", {
+    // So we can get `requestAnimationFrame`
+    pretendToBeVisual: true,
+})
+
+// Fill in the globals Mithril.js needs to operate. Also, the first two are often
+// useful to have just in tests.
+global.window = dom.window
+global.document = dom.window.document
+global.requestAnimationFrame = dom.window.requestAnimationFrame
+
+// And now, make sure JSDOM ends when the tests end.
+after(function() {
+    dom.window.close()
+})
