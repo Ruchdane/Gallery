d9d223f16942090bab0ac1a76ff5c21741cea35e
ruchdane <ruchdaneabiodun@gmail.com>
2022-07-06 11:32:31 +0100

    fixed jsx not linted or formated

--- src-mithril/component/header/header.jsx
+++ src-mithril/component/header/header.jsx
@@ -1,27 +1,40 @@
-import m from 'mithril';
-import Button from '../button/button';
-import './header.scss'
+import m from "mithril";
+import Button from "../button/button";
+import "./header.scss";
 
 const header = {
-	view(vnode) {
-		return <header>
-			<div class='info'>
-				<m.route.Link href="/galeries">
-					<img src="favicon.svg" width="38" height="30" c alt="Galery" loading="lazy" />
-				</m.route.Link>
-				<span >{vnode.attrs.label || ''}</span>
-			</div>
-			<div class='actions'>
-					{
-						vnode.attrs.actions.map(
-								action => <Button type="button" tooltip_modifier={action.tooltip_modifier} tooltip={action.tooltip}  class="secondary" onclick={action.perform}>
-											<i class={`bi bi-${action.icon}`}/>
-										</Button>
-						)
-					}
-			</div>
-
-		</header>
-	}
-}
-export default header
+  view(vnode) {
+    return (
+      <header>
+        <div class="info">
+          <m.route.Link href="/galeries">
+            <img
+              src="favicon.svg"
+              width="38"
+              height="30"
+              c
+              alt="Galery"
+              loading="lazy"
+            />
+          </m.route.Link>
+          <span>{vnode.attrs.label || ""}</span>
+        </div>
+        <div class="actions">
+          {vnode.attrs.actions.map((action, id) => (
+            <Button
+              type="button"
+              key={id}
+              tooltip_modifier={action.tooltip_modifier}
+              tooltip={action.tooltip}
+              class="secondary"
+              onclick={action.perform}
+            >
+              <i class={`bi bi-${action.icon}`} />
+            </Button>
+          ))}
+        </div>
+      </header>
+    );
+  },
+};
+export default header;
