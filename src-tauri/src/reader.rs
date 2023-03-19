b3295176ad7d6b89b6d27936e9d4b456bd5f81e4
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-03-19 11:58:22 +0000

    mock for article factory

--- src-tauri/src/reader.rs
+++ src-tauri/src/reader.rs
@@ -18,3 +18,43 @@ pub trait ArticleComponentFactory {
     fn create_chapter(&self, uri: &Uri) -> Result<Box<dyn Chapter>>;
     fn create_page(&self, uri: &Uri) -> Result<Page>;
 }
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    pub struct FakeFactory;
+    pub struct FakeBook;
+    pub struct FakeChapter;
+
+    impl ArticleComponentFactory for FakeFactory {
+        fn create_book(&self, uri: &Uri) -> Result<Box<dyn Book>> {
+            // implementation for creating a book
+            Ok(Box::new(FakeBook))
+        }
+
+        fn create_chapter(&self, uri: &Uri) -> Result<Box<dyn Chapter>> {
+            // implementation for creating a chapter
+            Ok(Box::new(FakeChapter))
+        }
+
+        fn create_page(&self, uri: &Uri) -> Result<Page> {
+            // implementation for creating a page
+            Ok(Page::Manga(PathBuf::from("page_path")))
+        }
+    }
+
+    impl Book for FakeBook {
+        fn get_chapter(&self, factory: &dyn ArticleComponentFactory) -> Vec<Box<dyn Chapter>> {
+            // implementation for getting chapters
+            vec![Box::new(FakeChapter)]
+        }
+    }
+
+    impl Chapter for FakeChapter {
+        fn get_page(&self, factory: &dyn ArticleComponentFactory) -> Vec<Page> {
+            // implementation for getting pages
+            vec![Page::Manga(PathBuf::from("page_path"))]
+        }
+    }
+}
